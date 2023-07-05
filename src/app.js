db2b8a13e5d06e3175b25bc4e127b1dd6f5ebb53
Roger Chung <84298258+rch-goldsnaker@users.noreply.github.com>
2023-07-05 15:41:18 -0300

    deleeting vercel json and config app.js

--- src/app.js
+++ src/app.js
@@ -25,14 +25,14 @@ app.use("/api/auth", authRoutes);
 app.use("/api", accountRoutes);
 app.use("/api", transferRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  const path = await import("path");
-  app.use(express.static("client/dist"));
+// if (process.env.NODE_ENV === "production") {
+//   const path = await import("path");
+//   app.use(express.static("client/dist"));
 
-  app.get("*", (req, res) => {
-    console.log(path.resolve("client", "dist", "index.html"));
-    res.sendFile(path.resolve("client", "dist", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     console.log(path.resolve("client", "dist", "index.html"));
+//     res.sendFile(path.resolve("client", "dist", "index.html"));
+//   });
+// }
 
 export default app;
